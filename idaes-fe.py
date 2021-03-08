11108ba44922140b24ad5d40ebe57848bef4b9d3
Tim Fuller <tim.fuller65@gmail.com>
2021-03-07 21:16:00 -0500

    Initial check in

---
+++ idaes-fe.py
@@ -0,0 +1,24 @@
+#######################################################################
+#                                                                     #
+#               IDAES Flowsheet Editor (IDAES-FE)                     #
+#                                                                     #
+#  This program is a graphical interface for creating, editing, and   #
+#  running process model flowsheets based on the DOE IDAES platform.  #
+#                                                                     #
+#  Author:  Timothy A. Fuller                                         #
+#  Date:  3/7/2020                                                    #
+#                                                                     #
+#  Version:  1.0.0                                                    #
+#                                                                     #
+#######################################################################
+
+
+# System module imports
+import sys
+import os
+
+# PyQT imports
+
+
+# Add parent directory to the system path
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
