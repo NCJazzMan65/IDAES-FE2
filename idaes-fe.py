ca19d10174e2987ebf7f0a1c4b066e61724ca2c0
Tim Fuller <tim.fuller65@gmail.com>
2021-03-08 20:05:58 -0500

    Main window work

--- idaes-fe.py
+++ idaes-fe.py
@@ -18,7 +18,46 @@ import sys
 import os
 
 # PyQT imports
-
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui as qtg
+from PyQt5 import QtWidgets as qtw
 
 # Add parent directory to the system path
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
+
+
+# Define main window class
+class MainWindow(qtw.QMainWindow):
+
+    # Initialize class
+    def __init__(self):
+        
+        # Call initialization on parent object
+        super().__init__()
+
+        # Setup main window
+        self.setWindowTitle("IDAES Flowsheet Editor")
+        self.centralWidget = qtw.QWidget(self)
+        self.resize(1024,768)
+        
+
+        # Show the main window
+        self.show()
+
+
+        
+
+
+
+# Define main method
+def main():
+
+    # Create the application and open the main window
+    app = qtw.QApplication(sys.argv)
+    mw = MainWindow()
+    sys.exit(app.exec())
+
+
+# Call main function on startup
+if __name__ == '__main__':
+    main()
